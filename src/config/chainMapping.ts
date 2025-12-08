7e37fc57aff8be3b2d286af34461567edfa9b23a
Huỳnh Nguyễn Văn Duy <duyhynh17@gmail.com>
2025-12-08 17:13:14 +0700

    feat: ux improvement (#12)
    * feat: ux improvement
    
    * fix: icon polkaVM

--- src/config/chainMapping.ts
+++ src/config/chainMapping.ts
@@ -1,4 +1,5 @@
-import { POLKAVM_CHAINS } from "@/constants";
+
+import { useChains } from "@luno-kit/react";
 
 // Mapping Substrate → EVM
 export const SUBSTRATE_TO_EVM: Record<string, number> = {
@@ -14,9 +15,34 @@ export const EVM_TO_SUBSTRATE: Record<number, string> = {
   420420422: "PAassetHub",
 };
 
-export const EVM_CHAIN_ICONS: Record<number, string> = {
-  420420422: POLKAVM_CHAINS.passet.chainIconUrl,
-  420420421: POLKAVM_CHAINS.wah.chainIconUrl,
-  420420418: POLKAVM_CHAINS.kah.chainIconUrl
+// Map chainId PolkaVM → genesisHash chain Polkadot 
+export const POLKAVM_TO_POLKADOT: Record<number, string> = {
+  420420418: "0x48239ef607d7928874027a43a67689209727dfb3d3dc5e5b03a39bdc2eda771a",
+  420420421: "0x67f9723393ef76214df0118c34bbbd3dbebc8ed46a10973a8c969d48fe7598c9",
+  420420422: "0xfd974cf9eaf028f5e44b9fdd1949ab039c6cf9cc54449b0b60d71b042e79aeb6",
 };
+
+export function useEvmChainIcons() {
+  const chains = useChains();
+
+  const iconMap: Record<number, string> = {};
+
+  chains.forEach((polkadotChain) => {
+    // Match genesisHash → chainId
+    const match = Object.entries(POLKAVM_TO_POLKADOT).find(
+      ([evmId, hash]) => hash === polkadotChain.genesisHash
+    );
+
+    if (match) {
+      const evmChainId = Number(match[0]);
+
+      //icon Polkadot chain
+      if (polkadotChain.chainIconUrl) {
+        iconMap[evmChainId] = polkadotChain.chainIconUrl;
+      }
+    }
+  });
+
+  return iconMap;
+}
 
